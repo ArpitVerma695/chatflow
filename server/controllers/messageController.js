61c8b2599c665ec9db26be88f4da1de35063a91e
Arpit Verma <arpitverma9005@gmail.com>
2026-07-12 04:38:29 +0530

    Fix Socket IO

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -27,7 +27,7 @@ export const sendMessage = async (req, res) => {
       username: username.trim(),
       message: message.trim(),
     });
-
+    const io = getIO();
     io.emit("receive-message", newMessage);
 
     res.status(201).json(newMessage);
